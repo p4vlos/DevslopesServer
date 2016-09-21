74e0dd10c578227b6604fdc1682b8e47af8aa365
p4vlos <nicolaou_paul@hotmail.com>
2016-09-21 10:33:04 +0100

    made the webview responsive to every iphone screen size but got an error with performing segue

---
+++ DevslopesTutorials/VideoVC.h
@@ -0,0 +1,14 @@
+//
+//  VideoVC.h
+//  DevslopesTutorials
+//
+//  Created by Pavlos Nicolaou on 21/09/2016.
+//  Copyright © 2016 Pavlos Nicolaou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Video;
+
+@interface VideoVC : UIViewController
+@property(nonatomic, strong) Video *video;
+@end
